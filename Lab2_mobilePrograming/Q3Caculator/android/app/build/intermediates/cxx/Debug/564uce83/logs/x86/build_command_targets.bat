830b814237c0d1bace3e66d11b3a4e5836f8fe88
Minh-12345 <minh.doduy.cit22@eiu.edu.vn>
2025-10-24 10:50:18 -0700

    Lab2

--- Lab2_mobilePrograming/Q3Caculator/android/app/build/intermediates/cxx/Debug/564uce83/logs/x86/build_command_targets.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-"C:\\Users\\ADMIN\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\ADMIN\\Q3Caculator\\android\\app\\.cxx\\Debug\\564uce83\\x86" ^
-  appmodules ^
-  react_codegen_safeareacontext
