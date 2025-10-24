d1f6e6aed3f8bb2f425ac94c652e576ab66ae5d2
Minh-12345 <minh.doduy.cit22@eiu.edu.vn>
2025-10-24 10:43:35 -0700

    Lab2

---
+++ Lab2_mobilePrograming/Q3Caculator/android/app/build/intermediates/cxx/Debug/564uce83/logs/arm64-v8a/build_command_targets.bat
@@ -0,0 +1,6 @@
+@echo off
+"C:\\Users\\ADMIN\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\ADMIN\\Q3Caculator\\android\\app\\.cxx\\Debug\\564uce83\\arm64-v8a" ^
+  appmodules ^
+  react_codegen_safeareacontext
---
+++ Lab2_mobilePrograming/Q3Caculator/android/app/build/intermediates/cxx/Debug/564uce83/logs/x86/prefab_command.bat
@@ -0,0 +1,22 @@
+@echo off
+"C:\\Program Files\\Microsoft\\jdk-17.0.16.8-hotspot\\bin\\java" ^
+  --class-path ^
+  "C:\\Users\\ADMIN\\.gradle\\caches\\modules-2\\files-2.1\\com.google.prefab\\cli\\2.1.0\\aa32fec809c44fa531f01dcfb739b5b3304d3050\\cli-2.1.0-all.jar" ^
+  com.google.prefab.cli.AppKt ^
+  --build-system ^
+  cmake ^
+  --platform ^
+  android ^
+  --abi ^
+  x86 ^
+  --os-version ^
+  24 ^
+  --stl ^
+  c++_shared ^
+  --ndk-version ^
+  27 ^
+  --output ^
+  "C:\\Users\\ADMIN\\AppData\\Local\\Temp\\agp-prefab-staging3936811521185434014\\staged-cli-output" ^
+  "C:\\Users\\ADMIN\\.gradle\\caches\\9.0.0\\transforms\\1e84847373c8b60ddb3f088099977c2a\\transformed\\react-android-0.82.1-debug\\prefab" ^
+  "C:\\Users\\ADMIN\\.gradle\\caches\\9.0.0\\transforms\\2b2a34f399e6566a4bc7f9555d03a28a\\transformed\\hermes-android-0.82.1-debug\\prefab" ^
+  "C:\\Users\\ADMIN\\.gradle\\caches\\9.0.0\\transforms\\5fa135f9220cf0ae1e3ee40cd371125b\\transformed\\fbjni-0.7.0\\prefab"
